3c56284a4d49bf755b56a0386818f6383a1b3bcc
amshunaik <103670494+amshunaik@users.noreply.github.com>
2024-07-15 01:43:48 +0530

    demo 2

--- src/app/health.service.ts
+++ src/app/health.service.ts
@@ -4,59 +4,10 @@ import { Healthdata } from './healthdata';
   providedIn: 'root'
 })
 export class HealthService {
-  // Data:Healthdata[]=[
-  //   {
-  //     id: 1,
-  //     name: 'John Doe',
-  //     workouts: [
-  //       { type: 'Running', minutes: 30 },
-  //       { type: 'Cycling', minutes: 45 }
-  //     ]
-  //   },
-  //   {
-  //     id: 2,
-  //     name: 'Jane Smith',
-  //     workouts: [
-  //       { type: 'Swimming', minutes: 60 },
-  //       { type: 'Running', minutes: 20 }
-  //     ]
-  //   },
-  //   {
-  //     id: 3,
-  //     name: 'Mike Johnson',
-  //     workouts: [
-  //       { type: 'Yoga', minutes: 50 },
-  //       { type: 'Cycling', minutes: 40 }
-  //     ]
-  //   },
-  //   {
-  //     id: 1,
-  //     name: 'cushio Doe',
-  //     workouts: [
-  //       { type: 'Running', minutes: 30 },
-  //       { type: 'Cycling', minutes: 45 }
-  //     ]
-  //   },
-  //   {
-  //     id: 1,
-  //     name: 'rohan Doe',
-  //     workouts: [
-  //       { type: 'Running', minutes: 30 },
-  //       { type: 'Cycling', minutes: 45 }
-  //     ]
-  //   },
-  //   {
-  //     id: 1,
-  //     name: 'Jotimhn Doe',
-  //     workouts: [
-  //       { type: 'Running', minutes: 30 },
-  //       { type: 'Cycling', minutes: 45 }
-  //     ]
-  //   },
-  // ];
   
 
   id1=4;
+  // Getting the workout list from localstorage
   getAllHealthData():Healthdata[]{
     let storedData = localStorage.getItem('healthdata');
     let parsedData
@@ -71,45 +22,7 @@ export class HealthService {
     return parsedData;
 
   }
-  // submitApplication(name: string, workout_type: string, workout_time: string) {
-  //   // console.log(
-  //   //   `Homes application received: username: ${name}, workout_type: ${workout_type}, workout_time: ${workout_time}.`,
-  //   // );
-  //   let user1:Healthdata={
-  //     id:this.id1,
-  //     name:name,
-  //     workouts:[
-  //       {type:workout_type,minutes:parseInt(workout_time)}
-  //     ]
-  //   };
-    
-  //   const exists=this.Data.find(d=>d.name.toLowerCase()===user1.name.toLowerCase());
-  //   console.log("uhgfcv : ",exists);
-  //   if(!exists){
-  //     this.Data.push(user1);
-  //     this.id1++;
-  //   }
-  //   else{
-  //     let g=exists.workouts.find(d=>d.type===user1.workouts[0].type);
-  //         if(g){
-  //           user1.workouts[0].minutes+=g.minutes;
-  //           //console.log("updated user  :",user1);
-  //           const index = exists.workouts.indexOf(g);
-  //           if (index > -1) {
-  //             exists.workouts.splice(index, 1);
-  //           }
-  //           //console.log("updated data : " ,exists)
-  //         }
-
-  //     exists.workouts.push(user1.workouts[0]);
-  //     console.log("final ans : ",exists);
-  //   }
-    
-  //   localStorage.setItem("healthdata",JSON.stringify(this.Data))
-  //   console.log("Data stored : ",this.Data)
-    
-  // }
-
+ // Adding data in localStorage
   constructor() {
     let r=localStorage.getItem('healthdata');
     if(r){
@@ -167,6 +80,5 @@ export class HealthService {
     }
     
 
-    //localStorage.setItem("healthdata",JSON.stringify(this.Data))
   }
 }
